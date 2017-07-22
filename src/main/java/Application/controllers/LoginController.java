dc66800573d4be954d1dc4d43361cbf5fcbbf702
anthonyliriano <tony@anthonyliriano.com>
2017-07-22 13:31:32 -0400

    Adding Post method for login controller.

--- src/main/java/Application/controllers/LoginController.java
+++ src/main/java/Application/controllers/LoginController.java
@@ -1,6 +1,10 @@
 package Application.controllers;
 
+import Application.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,7 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-    public String getLoginPage(){
+    public String getLoginPage(Model model){
+        model.addAttribute("User", new User());
         return "login";
+    }
+    @PostMapping("/login")
+    public String autheticateUser(@ModelAttribute User user){
+        System.out.println(user.getUsername());
+        return "home";
     }
 }
