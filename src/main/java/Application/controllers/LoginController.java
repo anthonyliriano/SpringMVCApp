8144501106379e42c4af98a47b9d6fc0e216c87e
anthonyliriano <tony@anthonyliriano.com>
2017-07-23 12:41:28 -0400

    Code clean-up: Removing useless output

--- src/main/java/Application/controllers/LoginController.java
+++ src/main/java/Application/controllers/LoginController.java
@@ -15,12 +15,10 @@ public class LoginController {
     @RequestMapping("/login")
     public String getLoginPage(Model model){
         model.addAttribute("User", new User());
-        System.out.println("Get '/' called");
         return "login";
     }
     @PostMapping("/login")
     public String autheticateUser(@ModelAttribute User user){
-        System.out.println("Post '/' called");
         return "login";
     }
 }
