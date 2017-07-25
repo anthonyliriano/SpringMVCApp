961bba058ea98384bb359fa5da503fa48e443dff
anthonyliriano <tony@anthonyliriano.com>
2017-07-25 12:33:45 -0400

    Fixing the HomeController test. It's Failing due not autowiring HomeController/PostService

--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -1,23 +1,47 @@
+import Application.config.RootConfig;
+import Application.config.WebAppInitializer;
+import Application.config.WebConfig;
 import Application.controllers.HomeController;
+import Application.services.PostService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by aliriano on 7/17/17.
  */
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebConfig.class})
+@WebAppConfiguration
 public class ControllerTest {
 
-    @Test //Test the Home Controller
-    public void TestHomePage() throws Exception{
-        HomeController homeController = new HomeController();
+    @Autowired
+    HomeController homeController;
+
+   @Autowired
+    PostService postService;
+
+    @Test
+    public void TestHomePage2() throws Exception{
+       // HomeController homeController = new HomeController();
         MockMvc mockMvc = standaloneSetup(homeController).build();
 
-        mockMvc.perform(get("/")).andExpect(view().name("home"));
-    }
+        mockMvc.perform(get("/"))
+                .andExpect(view().name("home"))
+                .andExpect(model().attribute("Post",postService.getPost()));
 
+    }
 }
