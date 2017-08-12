8ba706731165e9db9c9e02d2314f80f5b10ca6bd
anthonyliriano <tony@anthonyliriano.com>
2017-08-12 15:29:31 -0400

    Allowing Spring Security to process multipart/form-data

--- src/main/java/Application/config/SecurityWebApplicationInitializer.java
+++ src/main/java/Application/config/SecurityWebApplicationInitializer.java
@@ -1,9 +1,17 @@
 package Application.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.multipart.support.MultipartFilter;
+
+import javax.servlet.ServletContext;
 
 /**
  * Created by aliriano on 7/22/17.
  */
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+
+    @Override //Allow the ability to process multipart/form-data
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext){
+        insertFilters(servletContext, new MultipartFilter());
+    }
 }
