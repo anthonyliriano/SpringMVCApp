4df5ba08557552b7ee6990739f7bb7edb0fcc46a
anthonyliriano <tony@anthonyliriano.com>
2017-09-04 17:35:24 -0400

    Removing unused import statement

--- src/main/java/Application/config/WebConfig.java
+++ src/main/java/Application/config/WebConfig.java
@@ -3,7 +3,6 @@ package Application.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -11,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 
 /**
  * Created by aliriano on 7/14/17.
